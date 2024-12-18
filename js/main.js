7987ebd6276cc25ded002f78000daf569161150e
caltamura1 <christian@tallwalltech.com>
2024-12-18 16:44:53 -0500

    Adding format changes to structure of website. Reorganizing

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// function updateArrows() {
+//     arrowLeft.style.display = scrollContainer.scrollLeft <= 0 ? 'none' : 'block';
+//     // Maximum scroll position: scrollWidth (entire content) - clientWidth (visible part)
+//     arrowRight.style.display = 
+//       scrollContainer.scrollLeft >= (scrollContainer.scrollWidth - scrollContainer.clientWidth)
+//       ? 'none'
+//       : 'block';
+//   }
+  
+//   // Update arrow visibility on each scroll event
+//   scrollContainer.addEventListener('scroll', updateArrows);
+//   // Run once on load
+//   updateArrows();
